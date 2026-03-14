141bf613bbb895b33c97336710456bd1e1a1c476
Zdenek Gerhard <zdenekgerhard15@seznam.cz>
2026-03-14 21:56:51 +0100

    chrome options added to run headless and improve CI run

--- src/test/java/utils/DriverFactory.java
+++ src/test/java/utils/DriverFactory.java
@@ -2,6 +2,7 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
@@ -10,10 +11,17 @@ public class DriverFactory {
     public static WebDriver getDriver() {
 
         if (driver == null) {
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-        }
 
+            ChromeOptions options = new ChromeOptions();
+
+            // required for CI
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+
+            driver = new ChromeDriver(options);
+        }
         return driver;
     }
 
